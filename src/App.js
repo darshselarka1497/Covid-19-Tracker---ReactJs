9219e7612d2fb6fc851fa82e064da2a26ec28217
dselarka <dselarka@iu.edu>
2020-05-15 23:23:03 -0400

    Fetch active cases done

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import {Cards, Chart, CountryPicker} from './components';
+
+import styles from './App.module.css';
+
+import {fetchdata} from './api';
+
+class App extends React.Component{
+
+    state={
+        data:{},
+    }
+
+    async componentDidMount(){
+        const fetch = await fetchdata();
+        console.log(fetch);
+        this.setState({data:fetch});
+    }
+
+    render(){
+        const {data} = this.state;
+        return(
+            <div className={styles.container}>
+                <Cards data={data}/>
+                <CountryPicker/>
+                <Chart />
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Card, CardContent, Typography, Grid} from '@material-ui/core'
+import styles from './Cards.module.css'
+
+const Cards = ( {data: {confirmed, recovered, deaths, lastUpdate} } ) => {
+    
+    if(!confirmed){
+        return 'Loading...';
+    }
+    
+    return(
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" guttorBottom>Infected</Typography>
+                        <Typography variant="h5">{confirmed.value}</Typography> 
+                        <Typography color="textSecondary"> REAL DATE</Typography>
+                        <Typography color="body2">Number of Active Cases</Typography> 
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" guttorBottom>Recovered</Typography>
+                        <Typography variant="h5">REAK DATA</Typography> 
+                        <Typography color="textSecondary"> REAL DATE</Typography>
+                        <Typography color="body2">Number of Recoveries</Typography> 
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" guttorBottom>Deaths</Typography>
+                        <Typography variant="h5">REAK DATA</Typography> 
+                        <Typography color="textSecondary"> REAL DATE</Typography>
+                        <Typography color="body2">Number of Deaths</Typography> 
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
+
+export default Cards
